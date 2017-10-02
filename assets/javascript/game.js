3858f32d8e4ac036c7d930e1eef8f1a4c7e4f3cb
Sam <sjdefrank91@gmail.com>
2017-10-01 21:21:22 -0500

    no more star wars, layout sucks less now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,26 @@
-var states = ["select attacker", "select defender", "fighting"];
+var states = ["select player", "select opponent", "fighting"];
 var state = states[0];
+var picked = [];
 
 
 function characterCreator(name, hp, atk_pts){
     output = {};
     output.base_atk = atk_pts;
     output.name = name;
-    output.id = name.split(" ")[0];
+    output.id = name.split(" ")[0].toLowerCase();
     output.hp = hp;
     output.atk_pts = atk_pts;
-    output.img = output.id + ".jpg"
+    output.img = output.id + ".png"
     return output;
     }
 
-var luke = characterCreator("Luke Skywalker", 150, 40);
-var boba = characterCreator("Boba Fett", 10, 2);
-var jaba = characterCreator("Jabba the Hutt", 500, 3);
-var vader = characterCreator("Darth Vader", 200, 50);
-var han = characterCreator("Han Solo", 100, 40);
+var red = characterCreator("Red Knight", 150, 40);
+var orange = characterCreator("Orange Knight", 10, 2);
+var green = characterCreator("Green Knight", 500, 3);
+var blue = characterCreator("Blue Knight", 200, 50);
+var grey = characterCreator("Grey Knight", 100, 40);
 
-var char_array = [luke, boba, jaba, vader, han]
+var char_array = [red, orange, green, blue, grey];
 
 function fillLobby(chars){
     var img_path = "assets/images/"
@@ -29,10 +30,10 @@ function fillLobby(chars){
         chrDiv = $("<div class='card' id=" + chars[i].id + "></div>");
 
         //fill innerHTML of div (there has got to be a better way to do this)
-        chrDiv.html("<h3>" + chars[i].name + "</h3>" +
+        chrDiv.html("<p>" + chars[i].name + "</p>" +
                     "<img src='" + img_path + chars[i].img +"'/>" + "<br>" +
-                    "<div class='left'> HP: " + chars[i].hp + "</div>" +
-                    "<div class='right'>ATK: " + chars[i].base_atk + " </div>")
+                    "<div class='cardstat'>HP: " + chars[i].hp + "</div>" +
+                    "<div class='cardstat'>ATK: " + chars[i].base_atk + "</div>")
 
         //append chrDiv to lobby           
         $("#lobby").append(chrDiv);
@@ -41,38 +42,45 @@ function fillLobby(chars){
 }
 
 function select(card){
+    var id = "#" + card;
     var cardDiv = $("#" + card);
-    if(state == "select attacker"){
-        cardDiv.addClass("player");
-        $("#attacker").append(cardDiv);
+    if(picked.includes(id)){return};
+
+    picked.push(id);
+    
+    if(state == "select player"){
+
+        //add user class to selected character and move him to the arena
+        cardDiv.addClass("user picked hide_ovf");
+        $("#player").append($('.user'));
+
+        //set state to 'select opponent'
         state = states[1];  
     }
-    else if(state == "select defender"){
-        cardDiv.addClass("defender");
-        $("#defender").append(cardDiv);
+    
+    else if(state == "select opponent"){
+
+        //add opponent class to selected character, and move him to the arena
+        cardDiv.addClass("enemy picked hide_ovf");
+        $("#opponent").append(cardDiv);
+
+        //fade out remaining characters
+        $("#lobby").children().addClass("faded");
+
+        //place 'vs' image on the scree between the two fighters
+        $("#vs img").attr("src", "assets/images/vs.png");
+        $("#atk_btn").removeClass("faded");
+
+
+
+
+        //set state to 'fighting'
         state = states[2];
-    }
-}
+        }
+    else{return};
+};
 
 
-function selectAttacker(character){
-    if(fightInProgress){return};
-    //set class attacker
-    //move to arena
-    //populate stats
-    //stuff happens
-}
-
-function selectDefender(character){
-    if(fightInProgress){return};
-    //set class defender
-    //move to arena
-        //delete from current div
-        //append to arena
-    //populate stats
-    //stuff happens
-    fightInProgress = true;
-}
 
 function updateStats(attacker, defender){
     //update the scoreboard after each press of the attack button
